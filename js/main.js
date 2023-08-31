4a90de5769c6fbb739bc551b5d9d59bca3a07ddb
IreneChicca <irene.chicca@outlook.com>
2023-08-31 14:19:47 +0200

    Create array todolist

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-alert("ok");
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      todoList: [
+        { text: "Fare la spesa", done: false },
+        { text: "Pulire salone", done: false },
+        { text: "Ritirare pacco", done: false },
+        { text: "Andare in farmacia", done: false },
+      ],
+    };
+  },
+}).mount("#app");
